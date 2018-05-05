4e150726c1cd2d2cc4a349a07426b35e8a889705
Xu Lei <alivelee6@gmail.com>
2018-05-05 18:39:56 +0800

    add tag

--- src/components/Posts/PostItemDetail/index.js
+++ src/components/Posts/PostItemDetail/index.js
@@ -3,7 +3,9 @@ import * as actionTypes from '../../../types'
 import Grid from 'material-ui/Grid'
 import { withStyles } from 'material-ui/styles'
 import Paper from 'material-ui/Paper'
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card'
 import Typography from 'material-ui/Typography'
+import Chip from 'material-ui/Chip'
 import Loading from '../../Loading'
 import RelativePostItem from './relativePostItem'
 import shortid from 'shortid'
@@ -20,6 +22,15 @@ const styles = theme => ({
   },
   title: {
     marginBottom: '20px'
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%'// 16:9
+  },
+  chip: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 1.2
   }
 })
 
@@ -37,7 +48,7 @@ class PostItemDetail extends React.Component {
       type: actionTypes.CLEAR_POST_CACHE
     })
   }
-  componentDidUpdate(nextProps) {
+  componentDidUpdate (nextProps) {
     console.log('updated')
     let oldPostId = this.props.match.params.postId
     let newPostId = nextProps.match.params.postId
@@ -57,13 +68,28 @@ class PostItemDetail extends React.Component {
 
   render () {
     const { classes, posts: { postDetail } } = this.props
-    console.log(this.props)
+    // console.log(postDetail.screenshot_url['800px'])
     return (
       <React.Fragment>
         {!postDetail && <Loading />}
         {postDetail && <Grid container spacing={24} justify="center" className={classes.wrapper}>
           <Grid item xs={12} sm={7} md={7}>
-            <Paper className={classes.paper}>{postDetail.name}</Paper>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                image={postDetail.screenshot_url['850px']}
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="headline" component="h2">
+                  {postDetail.name}
+                </Typography>
+                {postDetail.topics.map(item => <Chip label={item.name} className={classes.chip} key={shortid.generate()}/>)}               
+                <Typography component="p">
+                  {postDetail.description}
+                </Typography>
+              </CardContent>
+            </Card>
           </Grid>
           <Grid item xs={12} sm={4} md={4}>
             <Typography variant="title" className={classes.title}>
